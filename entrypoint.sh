c19ac929e6c27a908bca69d560467353059f5d45
Tobi <tobias.weber@enisyst.de>
2020-01-23 13:21:44 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,14 +29,26 @@ update_review_request() {
     -H "${AUTH_HEADER}" \
     -H "${API_HEADER}" \
     -X $1 \
-    -d "{\"reviewers\":[\"${assignee}\"]}" \
+    -d "{\"body\":\"$1\"}" \
     "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${number}/requested_reviewers"
+}
+
+set_comment() {
+  curl -sSL \
+    -H "Content-Type: application/json" \
+    -H "${AUTH_HEADER}" \
+    -H "${API_HEADER}" \
+    -X POST \
+    -d "{\"reviewers\":[\"${assignee}\"]}" \
+    "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${number}/comments"
 }
 
 if [[ "$action" == "assigned" ]]; then
   update_review_request 'POST'
+  update_review_request "assigned ${assignee}"
 elif [[ "$action" == "unassigned" ]]; then
   update_review_request 'DELETE'
+  update_review_request 'unassigned ${assignee}'
 else
   echo "Ignoring action ${action}"
   exit 0
